966dce11c5a7a5e5ecde1ceb6d6e5c490e06d03a
Juan Pa Bedoya V <bedoyavascojuanpablo@gmial.com>
2023-12-13 02:39:23 -0500

    📦 NEW: Add .env file and NextAuth configuration Add SessionProvider component Add authentication options and handler Update RootLayout to include SessionProvider Add Card component and its sub-components Add login page and user login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,60 @@
 import React from 'react'
+import { UserLoginForm } from "./user-login-form"
+import Link from "next/link"
+import Image from "next/image"
 
 export default function LoginPage() {
   return (
-    <div>page</div>
+    <>
+      <div className="md:hidden">
+
+        <Image
+          src="/examples/authentication-light.png"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/authentication-dark.png"
+          width={1280}
+          height={843}
+          alt="Authentication"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Login an account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to Login your account
+              </p>
+            </div>
+            <UserLoginForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
